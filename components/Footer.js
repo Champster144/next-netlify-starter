38721296feba2feeb726722922694687aeff5c9e
Champster144 <140487664+Champster144@users.noreply.github.com>
2023-07-25 16:16:44 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="https://img.freepik.com/premium-vector/bored-panda-cute-kawaii-hand-drawn-doodle-bored-lazy-panda_42174-190.jpg" alt="Netlify Logo" className={styles.logo} />
       </footer>
     </>
   )
